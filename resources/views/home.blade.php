9f8c8099063098daeaef2fe1e900b176d09a94cb
RizkyMardiyansyah <rizkymardiyansyah23@gmail.com>
2025-06-14 16:04:50 +0700

    up

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -34,7 +34,7 @@
               </div>
             </div>
           </li> --}}
-          <li class="third">
+          {{-- <li class="third">
             <div class="slider-text-wrapper">
               <div class="container">
                 <h1 class="big" data-lang-en="Car Rental Service in Padang" data-lang-id="Jasa Rental Mobil Kota Padang">Jasa Rental Mobil Kota Padang</h1>
@@ -42,7 +42,7 @@
                 <a href="https://wa.me/6281268258986" target="_blank" class="middle btn btn-white-border" title="Jasa Rental Mobil Kota Padang" data-lang-en="Book Now" data-lang-id="Rental Sekarang">Rental Sekarang</a>
               </div>
             </div>
-          </li>
+          </li> --}}
         </ul>
       </div>
     </section>
