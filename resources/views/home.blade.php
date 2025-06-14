2a572ea162c7682eff7cb0118a0d5a27b4b171ec
RizkyMardiyansyah <rizkymardiyansyah23@gmail.com>
2025-06-14 15:48:39 +0700

    up

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,7 +18,7 @@
             <div class="slider-text-wrapper">
               <div class="container">
                 <h1 class="big" data-lang-en="Car Rental in Padang" data-lang-id="Rental Mobil di Kota Padang">Rental Mobil di Kota Padang</h1>
-                <h1 data-lang-en="Car Rental " data-lang-id="Rental Mobil ">Rental Mobil</h1>
+                <h1 style="color: white" data-lang-en="Car Rental " data-lang-id="Rental Mobil ">Rental Mobil</h1>
                 <h2 class="small" data-lang-en="Reliable car rental service for personal or business needs in Padang" data-lang-id="Layanan sewa mobil terpercaya untuk kebutuhan pribadi maupun bisnis di Padang">Layanan sewa mobil terpercaya untuk kebutuhan pribadi maupun bisnis di Padang</h2>
                 <a href="https://wa.me/6281268258986" target="_blank" class="middle btn btn-white-border" title="Pesan Rental Mobil di Padang" data-lang-en="Book Now" data-lang-id="Rental Sekarang">Rental Sekarang</a>
               </div>
